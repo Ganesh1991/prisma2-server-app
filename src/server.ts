b566aefe87a760f1cd11d092111edbc9a0d91dbf
Saket Tawde <saket.tawde@gmail.com>
2020-03-22 19:19:18 +0530

    deployment ready changes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,11 @@ import { GraphQLServer } from 'graphql-yoga'
 import { schema } from './schema'
 import { createContext } from './context'
 
+const opts = {
+  port: process.env.PORT || 4000,
+}
+
 new GraphQLServer({
   schema,
   context: createContext,
-}).start(() => console.log(`Server ready at: http://localhost:4000`))
+}).start(opts, ({port}) => console.log(`Server ready at port: ${port} || 4000`))
